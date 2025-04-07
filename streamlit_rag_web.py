d904c22771618fd0f44f2c7cb914cecf3e057080
surplush <118612920+surplush@users.noreply.github.com>
2025-04-07 17:06:32 +0900

    Update streamlit_rag_web.py

--- streamlit_rag_web.py
+++ streamlit_rag_web.py
@@ -20,7 +20,7 @@ sys.modules['sqlite3']=sys.modules.pop('pysqlite3')
 from langchain_chroma import Chroma
 
 #오픈AI API 키 설정
-os.environ["OPENAI_API_KEY"] = st.screts["OPENAI_API_KEY"]
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 #cache_resource로 한번 실행한 결과 캐싱해두기
 @st.cache_resource
 def load_and_split_pdf(file_path):
